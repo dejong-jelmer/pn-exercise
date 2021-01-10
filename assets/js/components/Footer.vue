f70a2f844ef532a9600064e257d9cc9c4d979d9b
jelmer de jong <j.de.jong@tutanota.com>
2021-01-10 16:14:33 +0100

    first commit

---
+++ assets/js/components/Footer.vue
@@ -0,0 +1,20 @@
+<template> 
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          Site door <a href="https://github.com/dejong-jelmer">{{ author }}</a> in opdracht van <a href="https://www.proudnerds.com/">{{ company }}</a>. &copy {{ date }}
+        </p>
+      </div>
+  </footer>
+</template>
+
+<script>
+    export default {
+        props: ['author', 'company', 'date']
+    }
+    console.log("Footer loaded");
+</script>
+
+<style>
+ 
+</style>
---
+++ assets/js/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <header id="header">
+        <div class="columns is-mobile is-centered">
+            <div class="column is-one-quarter">  
+                <div class="block p-4 mt-5">
+                    <h1 class="title">
+                        {{ author }}
+                    </h1>
+                    <p class="subtitle">
+                        A test for <strong>{{ company }}</strong>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        props: ['author', 'company']
+    }
+    console.log("Header loaded");
+</script>
+<style>
+    #header {
+        height: 100px;
+        position: relative;
+        background-color: lightgray;
+    }
+
+    #header .block {
+        position: absolute;
+
+        background-color: white;
+        border-radius: 10px;
+        border: 0.5px solid gray;
+        
+        z-index: 1;
+    }
+</style>
+
+
+
